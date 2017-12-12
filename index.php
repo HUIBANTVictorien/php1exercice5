d7cd37e8d848c2b10eb018bd03eafee967009ff8
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:07:16 +0100

    maj exo5

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-$number = NULL;
-echo $number.'<br/>';
-$number = 42;
-echo $number.'<br/>';
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 5 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $number = NULL;
+    echo $number.'<br/>';
+    $number = 42;
+    echo $number.'<br/>';
+    ?>
+  </body>
+</html>
